58231883aa8c8efcaf2c0eb8d94d9a3baef5b039
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-02-24 14:44:16 -0500

    finished the footer

--- tik_tok/src/Video.js
+++ tik_tok/src/Video.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import "./Video.css"
-
+import VideoFooter from "./VideoFooter";
 
 function Video() {
     const videoRef =  useRef(null);
@@ -23,9 +23,8 @@ onClick={handleVideoPress}
 className="video__player"
 loop
 ref={videoRef}
-src="https://v16-webapp.tiktok.com/9518c1e504a00d99717589f5293b9c04/62169728/video/tos/maliva/tos-maliva-ve-0068c799-us/feba99cecc5b4e48b10e70228792a091/?a=1988&br=1244&bt=622&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3E7nz7Thl2RUDXq&l=202202231420490101890720720ADA61E7&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anJmNzk6Zmt5OjMzZzczNEApaGg1NGY4PDxkNzMzMzZpN2c0cWcvcjRfcnNgLS1kMS9zcy40XjMyMF8tNl9fYi4yLTY6Yw%3D%3D&vl=&vr="></video>
+src="https://v16-webapp.tiktok.com/58d72512daf2367451bf97256c30f590/62183046/video/tos/useast2a/tos-useast2a-ve-0068c002/e83b0c216e464f13ac45fc0629b869b3/?a=1988&br=1206&bt=603&cd=0%7C0%7C0%7C0&ch=0&cr=0&cs=0&dr=0&ds=2&er=&ft=XOQ9-3E7nz7ThVFgUDXq&l=20220224192622010192053130145A1432&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anN1czM6ZnR3ODMzNzczM0ApZ2g6OmlkOmVkN2U6aGc1PGdyaTNucjRnYGBgLS1kMTZzcy1fYF42NGNgYDNjLS9eL2E6Yw%3D%3D&vl=&vr="></video>
 <VideoFooter />
-{/* VideoFooter */}
 {/*  VideoSidebar*/}
 
     </div>
