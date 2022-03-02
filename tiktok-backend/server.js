12f7fa573c85a7ac5f7dfc1b0f0c8d9f63b575b8
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-03-02 10:55:43 -0500

    added data, db modes, to connect database using mongoose

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,8 +1,41 @@
 import express from "express";
 import mongoose from "mongoose";
+import Data from "./data.js";
+import Videos from "./dbModel.js";
 
-const app= express();
+
+// app config
+const app = express();
 
 const port = 9000;
 
-app,get('/')
+// middlewares
+app.use(express.json())
+//DB config
+const connection_url = 'mongodb+srv://bml202290:Indeed123!@cluster0.bx4im.mongodb.net/tiktokdb?retryWrites=true&w=majority'
+mongoose.connect(connection_url, {
+ 
+});
+// api endpoints
+
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+
+app.get('/v1/posts', (req, res) => res.status(200).send(Data))
+
+app.post('/v2/posts', (req,res) => {
+
+    //POST request is to ADD data to data base
+    // It will let us ADD a video Document to the videos COLLECTION
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data) =>{
+        if(err) {
+            res.status(500).send(err)
+        } else{
+            res.status(201).send(data)
+        }
+    })
+})
+// listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
